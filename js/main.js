904f3ac546aaff83f3facf063e4ffed16f35ae8b
Abdoelashry21 <abdoelashry908@gmail.com>
2024-09-04 07:17:10 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,30 @@
+
+function myFun(){
+   var abdo = document.getElementById("doller").value;
+
+   var resul = document.getElementById("ahmed");
+
+   resul.innerHTML = abdo*50 + " " + "Egyption Pound";
+
+   return false;
+
+}
+
+// if(abdo==""){
+//     resul.innerHTML="please enter the number";
+//     return false;
+// }
+// else if(abdo<=0){
+//     resul.innerHTML="please enter a positive number";
+//     return false;
+// }
+// else if(isNaN(abdo)){
+//     resul.innerHTML="please enter a number";
+//     return false;
+// }
+// else{
+//     resul.innerHTML = abdo*50 + " " + "Egyption Pound";
+
+//     return false;
+
+// }
